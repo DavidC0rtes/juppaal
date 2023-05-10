846287902fb8c58b61c53d4e248c64f34148f8e4
Santiago Cortes <david.cortes@correounivalle.edu.co>
2023-05-10 12:09:03 +0200

    Add Channel class

---
+++ src/main/java/uppaal/declarations/Channel.java
@@ -0,0 +1,69 @@
+package uppaal.declarations;
+
+import java.util.Objects;
+
+public class Channel {
+    private final String name;
+    private final String scope;
+    private final ChanType type;
+    public Channel(String name, String scope, ChanType type) {
+        this.name = name;
+        this.type = type;
+        this.scope = scope;
+    }
+
+    public static enum ChanType {
+        BROADCAST,
+        URGENT,
+        BINARY
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (!(o instanceof Channel)) {
+            return false;
+        }
+
+        Channel chan = (Channel) o;
+
+        if (!this.name.equals(chan.getName())) {
+            return false;
+        }
+
+        if (!this.type.equals(chan.getType())) {
+            return false;
+        }
+
+        return this.scope.equals(chan.getScope());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, scope, type);
+    }
+
+    @Override
+    public String toString() {
+        return "Channel{" +
+                "name='" + name + '\'' +
+                ", scope='" + scope + '\'' +
+                ", type=" + type +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public ChanType getType() {
+        return type;
+    }
+}
